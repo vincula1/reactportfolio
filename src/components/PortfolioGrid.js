9eaa25278810f406f1789cc2ca0d0c9de50b547e
vincula <141301612+vincula1@users.noreply.github.com>
2023-12-10 14:57:40 -0600

    Added github urls to projects and made contact section

--- src/components/PortfolioGrid.js
+++ src/components/PortfolioGrid.js
@@ -4,14 +4,16 @@ import '../styles/PortfolioGrid.css';
 
 function PortfolioGrid() {
     const portfolioItems = [
-        { title: 'Connect 4 AI', imageUrl: 'https://www.hasbro.com/common/productimages/en_US/80FB5BCA50569047F5F45EB5DF88DAF4/16522e18bdd7c982a51711270822bee0d9d779e8.jpg' },
-        { title: 'Rizz-O-Matic', imageUrl: 'https://art.pixilart.com/8d266200ab7fe5a.png'},
+        { title: 'Connect 4 AI', imageUrl: 'https://www.hasbro.com/common/productimages/en_US/80FB5BCA50569047F5F45EB5DF88DAF4/16522e18bdd7c982a51711270822bee0d9d779e8.jpg', githubUrl: 'https://github.com/vincula1/connect4' },
+        { title: 'Rizz-O-Matic', imageUrl: 'https://art.pixilart.com/8d266200ab7fe5a.png', githubUrl: 'https://github.com/AllieStewart/Rizz-o-matic'},
+        { title: 'Weather Dashboard', imageUrl: 'https://play-lh.googleusercontent.com/4LVcW1MTvnxkw3Qjj-x8xMi61eZjRfUnZBr_CGjK32U1heM7vdsow3dSDW_l-oYQFg=w240-h480-rw', githubUrl: 'https://github.com/vincula1/weatherdash' },
+        { title: 'NBA Live Schedule', imageUrl: 'https://logo.com/image-cdn/images/kts928pd/production/87f7ea9c02c3d00e1763891534c6d2bd5846f5a7-731x731.png?w=1080&q=72', githubUrl: 'https://github.com/vincula1/nbaproject'},
     ];
 
     return (
         <div className="portfolio-grid">
             {portfolioItems.map((item, index) => (
-                <PortfolioItem key={index} title={item.title} imageUrl={item.imageUrl} />
+                <PortfolioItem key={index} title={item.title} imageUrl={item.imageUrl} githubUrl={item.githubUrl} />
             ))}
         </div>
     );
--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../styles/PortfolioItem.css';
 
-function PortfolioItem({ title, imageUrl }) {
+function PortfolioItem({ title, imageUrl, githubUrl }) {
     return (
-        <div className="portfolio-item">
+        <a href={githubUrl} className="portfolio-item" target="_blank" rel="noopener noreferrer">
             <img src={imageUrl} alt={title} />
             <div className="overlay">
                 <div className="text">{title}</div>
             </div>
-        </div>
+        </a>
     );
 }
+
 
 export default PortfolioItem;
